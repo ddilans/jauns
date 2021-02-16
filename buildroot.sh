ec71435d9dfbb217b64ca79f3b72230f00ac58e9
ddilans <ddilans@github>
2021-02-15 23:44:33 -0800

    minor fixes

--- buildroot.sh
+++ buildroot.sh
@@ -11,7 +11,6 @@ cp "${BUILD_DIR}/busybox-1.33.0/busybox" /mnt/jauns/bin/busybox
 cd bin && for t in $(./busybox --list); do ln -s busybox $t; done
 cd /mnt/jauns
 ln -s bin/busybox init
-cp "${BUILD_DIR}/xstow-1.0.2/src/xstow-static" /mnt/jauns/bin/xstow
 cp -r "${BUILD_DIR}"/files/* /mnt/jauns
 cd /mnt/jauns/dev
 mknod tty1 c 4 1
--- qemu.sh
+++ qemu.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+if [ "$1" != "raw" ]; then
 cp filesystem.img filesystem.img.mod
-#qemu-system-x86_64 -m 512 -drive file=filesystem.img.mod,format=raw,index=0,media=disk
-qemu-system-x86_64 -m 512 -drive file=filesystem.img.mod,if=virtio,format=raw,index=0,media=disk -serial stdio -cpu kvm64
+qemu-system-x86_64 -m 512 -drive file=filesystem.img.mod,if=virtio,format=raw,index=0,media=disk -serial stdio -cpu max
+else
+qemu-system-x86_64 -m 512 -drive file=filesystem.img,if=virtio,format=raw,index=0,media=disk -serial stdio -cpu max
+fi
